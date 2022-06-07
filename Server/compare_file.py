9a40ba394793b349498e365377fd0d77d27b35ad
adisocoletzky <91720787+adisocoletzky@users.noreply.github.com>
2022-06-07 21:43:23 +0300

    Add files via upload

--- Server/compare_file.py
+++ Server/compare_file.py
@@ -2,6 +2,7 @@ import difflib
 
 
 def compare_files(before, after):
+    #find the differnce between two data
     output_list = list(filter(lambda li: li[0] != ' ', difflib.ndiff(before, after)))
     delta = ''
     for ch in output_list:
